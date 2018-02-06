17614089ec94e57c350e264e547b90d1096d11d4
talamajo <35745097+talamajo@users.noreply.github.com>
2018-02-05 21:26:27 -0500

    Update StudentView.java

--- src/StudentView.java
+++ src/StudentView.java
@@ -1,29 +1,58 @@
 
 public class StudentView {
 	private Student student;
-	
-	public StudentView(Student student) {
-		this.student = student;
-	}
-	
-	public StudentView() {
-		
-	}
-	
-	public void printStudentDetails(Student student) {
-		this.student = student;
-		printStudentDetails();
-	}
-	
-	public void setStudentDetails(Student student) {
-	
-	}
-	
-	public void printStudentDetails() {
-		if (this.student == null) throw new IllegalArgumentException("student");		
 
-		System.out.println("Student's first name is " + this.student.getFirstName());
-		System.out.println("Student's last name is " + this.student.getLastName());
-		System.out.println("Student's id number is " + this.student.getIdNumber());
+
+	public StudentView (Student student) {
+
+		this.student = student;
+
 	}
+
+
+	public StudentView() {
+
+
+	}
+
+
+	public void printStudentDetails(Student student) {
+
+		this.student = student;
+
+		printStudentDetails();
+
+	}
+
+
+	public void setStudentDetails (Student student) {
+
+	}
+
+
+	public void printStudentDetails() {
+
+	if (this.student == null) throw new IllegalArgumentException("student");
+
+
+	if (this.student != null) {
+
+		System.out.println("Student’s first name is " + this.student.getFirstName());
+
+		System.out.println("Student’s last name is " + this.student.getLastName());
+
+		System.out.println("Student’s id number is "+ this.student.getIdNumber());
+		
+		System.out.println("Student’s Email is "+ this.student.getEmail());
+		
+		System.out.println("Student’s Courses is "+ this.student.getCourses());
+	
+		}
+
+	}
+
+
+
+
+
 }
